8da05dbf2d6c74a8d1116874d91df7ad36ed2bef
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-04 13:52:56 +0000

    routes

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,27 +2,12 @@ module.exports = app => {
     const users = require("../controllers/user.controller.js");
   
     var router = require("express").Router();
-  
+     
     // Create a new users
-    router.post("/", users.create);
-  
-    // Retrieve all users
-    router.get("/", users.findAll);
-  
-    // Retrieve all published users
-    router.get("/published", users.findAllPublished);
-  
-    // Retrieve a single users with id
-    router.get("/:id", users.findOne);
-  
-    // Update a users with id
-    router.put("/:id", users.update);
-  
-    // Delete a users with id
-    router.delete("/:id", users.delete);
-  
-    // Delete all users
-    router.delete("/", users.deleteAll);
+    router.post("/signup", users.signup);
+
+    router.post('/login', users.login);
+
   
     app.use('/api/users', router);
   };
