bd8cc64c6be6ae9113a58b1706ee42a6a29f87cd
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-04 12:27:30 +0000

    mysql setup

---
+++ app/server.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// const app = express();
+// app.use(...);
+
+const db = require("../app/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// app.use(...);
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to social media application." });
+});
+
+
+require("./routes/tutorial.routes")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+// const http = require('http');
+// const app = require('./app');
+
+// const normalizePort = val => {
+//   const port = parseInt(val, 10);
+
+//   if (isNaN(port)) {
+//     return val;
+//   }
+//   if (port >= 0) {
+//     return port;
+//   }
+//   return false;
+// };
+// const port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
+
+// const errorHandler = error => {
+//   if (error.syscall !== 'listen') {
+//     throw error;
+//   }
+//   const address = server.address();
+//   const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+//   switch (error.code) {
+//     case 'EACCES':
+//       console.error(bind + ' requires elevated privileges.');
+//       process.exit(1);
+//       break;
+//     case 'EADDRINUSE':
+//       console.error(bind + ' is already in use.');
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// };
+
+// const server = http.createServer(app);
+
+// server.on('error', errorHandler);
+// server.on('listening', () => {
+//   const address = server.address();
+//   const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+//   console.log('Listening on ' + bind);
+// });
+
+// server.listen(port);
+ 
